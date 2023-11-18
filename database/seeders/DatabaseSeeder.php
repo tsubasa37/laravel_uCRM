b4079da2ff195f7b359f2b947d5b9fce1b377d9d
tsubasa <tsuba@2637.com>
2023-11-18 18:20:14 +0900

    customerSearch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Purchase;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,10 +18,19 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ItemSeeder::class,
-            CustomerSeeder::class,
         ]);
 
 
         \App\Models\Customer::factory(1000)->create();
+
+        $items = \App\Models\Item::all();
+
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items) {
+            $purchase->items()->attach(
+                $items->random(rand(1,3))->pluck('id')->toArray(),
+                ['quantity' => rand(1, 5)]
+            );
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
 use Inertia\Inertia;
+use App\Http\Controllers\PurchaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,9 @@ Route::resource('items' , ItemController::class)
     ->middleware(['auth', 'verified']);
 
 Route::resource('customers' , CustomerController::class)
+    ->middleware(['auth', 'verified']);
+
+Route::resource('purchases' , PurchaseController::class)
     ->middleware(['auth', 'verified']);
 
 Route::get('/inertia/index', [InertiaTestController::class,'index'])->name('inertia/index');
